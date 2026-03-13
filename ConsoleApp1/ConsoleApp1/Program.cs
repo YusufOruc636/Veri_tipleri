8e0c3e0e4c1b9dce57198bd377f465d588f5e6b9
YusufOruc636 <yusuf_33.34@hotmail.com>
2026-03-13 20:34:22 +0300

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿// kur bilgileri ve tarih
+
+var dollarRate = 27.5m;
+decimal euroRate = 30.2m;
+decimal goldRate = 1800.75m;
+DateTime rateDate= DateTime.Now;
+
+Console.WriteLine("tl miktarınızı giriniz: ");
+decimal tlAmount = Convert.ToDecimal(Console.ReadLine());
+
+// menü
+
+Console.WriteLine("\n donüstürme işlemleri");
+Console.WriteLine("1- dolar");
+Console.WriteLine("2- euro");
+Console.WriteLine("3- altın");
+
+Console.WriteLine("secminizi yapınız: (1-3)");
+int choice=Convert.ToInt32(Console.ReadLine());
+
+decimal result = 0m;
+string currencyName = "";
+
+switch (choice)
+{
+    case 1:
+        result = tlAmount / dollarRate;
+        currencyName = "dolar";
+        break;
+    case 2:
+        result = tlAmount / euroRate;
+        currencyName = "euro";
+        break;
+    case 3:
+        result = tlAmount / goldRate;
+        currencyName = "altın";
+        break;
+    default:
+        Console.WriteLine("geçersiz seçim");
+        return;
+}
+
+Console.WriteLine($"\n tarih: {rateDate}");
+Console.WriteLine($"girilen tl miktarı {tlAmount}");
+Console.WriteLine($"donüsüm sonucu: {result:F2} {currencyName}");
---
+++ ConsoleApp1/İsaretliİşsaretsizbyt/Program.cs
@@ -0,0 +1,21 @@
+﻿// isaretsiz byte
+Console.WriteLine("isaretsiz byte (0-255)");
+Console.WriteLine($"min deger: {byte.MinValue}, max deger: {byte.MaxValue}");
+
+Console.WriteLine("sbyte (-128 - 128");
+Console.WriteLine($"min deger: {sbyte.MinValue}, max deger: {sbyte.MaxValue}");
+
+// isaretsiz degerlerde tasma
+Console.WriteLine("");
+byte unsignedValue = 255;
+Console.WriteLine($"isaretsiz byte degeri: {unsignedValue}");
+unsignedValue++;
+Console.WriteLine($"isaretsiz byte degeri (tasma sonrasi): {unsignedValue}");
+
+// ikili gosterim binary
+
+Console.WriteLine("binary karısıklıklar");
+Console.WriteLine($"Byte 255 : {Convert.ToString(255,2)}");
+Console.WriteLine($"Byte 255: {Convert.ToString(15,2).PadLeft(8,'0')}");
+
+Console.ReadKey();
